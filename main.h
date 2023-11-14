b6bf768775c580db0fe9578b708a20d37bf7305e
NchedoAnulika <nchedo.arinze@gmail.com>
2023-11-14 07:48:07 -0800

    Task 0

--- main.h
+++ main.h
@@ -5,6 +5,20 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+/**
+ * struct format - Printf conversion specifier
+ * @specifier: Type char pointer of the specifier
+ * @handler: Function for the conversion specifier
+ *
+ * Description: This structure pairs a conversion
+ * specifier with its corresponding handler function.
+ */
+typedef struct format
+{
+    char *specifier;
+    int (*handler)();
+} format;
+
 int _putchar(char c);
 int _printf(const char *format, ...);
 int printf_char(va_list args);
