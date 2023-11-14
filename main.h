76efc7e4fb845283ae8b794f1848869a9f615556
NchedoAnulika <nchedo.arinze@gmail.com>
2023-11-14 03:45:17 -0800

    Add main.h, a.out, _putchar.c and print_char_str.c

--- main.h
+++ main.h
@@ -14,14 +14,11 @@ int printf_string(va_list args);
 int print_signed_integer(int num);
 int print_binary(unsigned int num);
 int print_integer(int num);
-int print_chars(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
-int print_strings(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
 int print_percents(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 int print_ints(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 int print_binarys(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
+
 #endif
